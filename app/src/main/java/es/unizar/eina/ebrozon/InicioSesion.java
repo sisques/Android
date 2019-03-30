2a29f04f8f4f02138bd99c8c596edbf492c8ed79
victor <740138@unizar.es>
2019-03-30 20:27:36 +0100

    Inicio lógica LOGIN

--- app/src/main/java/es/unizar/eina/ebrozon/InicioSesion.java
+++ app/src/main/java/es/unizar/eina/ebrozon/InicioSesion.java
@@ -5,24 +5,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 
 public class InicioSesion extends AppCompatActivity {
 
+
+    String url = "";
+    RequestQueue queue = Volley.newRequestQueue(this);
+    //TUTORIAL https://www.itsalif.info/content/android-volley-tutorial-http-get-post-put
     private Button iniciar;
+    private Button olvidar;
+    private EditText userName;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_sesion);
 
-        iniciar = findViewById(R.id.iniciarSesion);
+
+        userName = findViewById(R.id.UserName_login);
+        password = findViewById(R.id.Password_login);
+        iniciar = findViewById(R.id.LogIn);
+        olvidar = findViewById(R.id.forgotPassword_login);
+    }
+
+    private Boolean validate (String UserName, String password){
+
     }
 
     //Este método cambia a la actividad de pantalla principal.
-    //TODO: Configurar el inicio de sesión antes de saltar a la pantalla principal
     public void iniciarSesion(View view){
-        startActivity(new Intent(InicioSesion.this, PantallaPrincipal.class));
+        if ( validate(userName.getText().toString(), password.getText().toString()) ) {
+            startActivity(new Intent(InicioSesion.this, PantallaPrincipal.class));
+        }
+        else{
+
+        }
     }
 
 }
